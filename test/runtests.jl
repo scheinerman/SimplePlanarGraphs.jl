47eb7d449e04f757dbf3ce777b8b83fe4c7bb269
Ed Scheinerman <ers@jhu.edu>
2020-09-26 09:58:10 -0400

    Update runtests

--- test/runtests.jl
+++ test/runtests.jl
@@ -4,7 +4,7 @@ G = Cube(3)
 embed(G,:combined)
 @test count_cross(G) == 2
 
-G = RandomTriangulation(10)
+G = RandomTriangulation(17)
 @test count_cross(G) == 0
 @test euler_char(G) == 2
 H = dual(G)
