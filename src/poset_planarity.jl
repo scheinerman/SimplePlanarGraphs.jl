dda40b5c5a5de956fc12dffef2ed21c55e509b8b
Ed Scheinerman <ers@jhu.edu>
2022-04-28 20:06:02 -0400

    Stub

---
+++ src/poset_planarity.jl
@@ -0,0 +1,9 @@
+using SimplePosets, SimplePosetAlgorithms
+
+using SimpleGraphs, SimplePlanarGraphs  # remove his later
+
+function is_planar(G::SimpleGraph)
+    # Create a poset from G and see if it has dimension ≤ 3.
+
+
+end
