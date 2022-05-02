dbe276505d92e36fb8c0a06872f86161a40b3593
Ed Scheinerman <ers@jhu.edu>
2022-05-02 12:22:13 -0400

    Documented

--- src/poset_planarity.jl
+++ src/poset_planarity.jl
@@ -1,7 +1,7 @@
 using SimplePosets, SimplePosetAlgorithms
 
 # using SimpleGraphs, SimplePlanarGraphs  # remove his later
-export is_planar
+export is_planar, schnyder_realizer
 
 """
     is_planar(G::SimpleGraph)
@@ -14,16 +14,42 @@ function is_planar(G::SimpleGraph)::Bool
     # Create a poset from G and see if it has dimension ≤ 3.
 
     if !quick_planar_check(G)
-        return false 
+        return false
     end
 
-    P = SimplePoset(G)
     try
-        R = realizer(P,3)
+        R = schnyder_realizer(G)
         return true
     catch
         return false
     end
     return false
-
 end
+
+"""
+    schnyder_realizer(G)
+
+Return an `n`-by-`3` matrix each of whose columns contains the vertices of `G`.
+This is a reduced version of a three-dimensional realizer of `P(G)`.
+
+Throws an error if `G` is not planar.
+"""
+function schnyder_realizer(G::SimpleGraph{T}) where {T}
+    if cache_check(G, :schnyder_realizer)
+        return cache_recall(G, :schnyder_realizer)
+    end
+
+    P = SimplePoset(G)
+    R = realizer(P, 3)
+    n = NV(G)
+
+    result = Array{T}(undef, n, 3)
+
+    for k = 1:3
+        c = R[:, k]
+        result[:, k] = [x for x in c if x isa T]
+    end
+    cache_save(G, :schnyder_realizer, result)
+    return result
+end
+
