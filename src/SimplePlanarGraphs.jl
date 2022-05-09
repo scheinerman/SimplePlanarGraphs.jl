bb5b036ea7d1ef054d6a1eb508aeefcc78098fdb
Ed Scheinerman <ers@jhu.edu>
2022-05-09 16:16:42 -0400

    Some caching

--- src/SimplePlanarGraphs.jl
+++ src/SimplePlanarGraphs.jl
@@ -13,15 +13,28 @@ If this function returns `false` the graph is definitely not planar. If it
 returns `true` it might be planar, but it might not.
 """
 function quick_planar_check(G::SimpleGraph)::Bool
+
+    if cache_check(G, :is_planar)
+        return cache_recall(G, :is_planar)
+    end
+
     n = NV(G)
     if n ≤ 4
+        cache_save(G, :is_planar, true)
         return true
     end
 
     g = girth(G)
     if g == 0
+        cache_save(G, :is_planar, true)
         return true
     end
+
+    if is_connected(G) && euler_char(G) == 2
+        cache_save(G, :is_planar, true)
+        return true
+    end
+
     m = NE(G)
     if m ≤ g * (n - 2) / (g - 2)
         return true
@@ -31,6 +44,7 @@ function quick_planar_check(G::SimpleGraph)::Bool
     if NV(H) == 0
         return true
     end
+    cache_save(G, :is_planar, false)
     return false
 end
 
